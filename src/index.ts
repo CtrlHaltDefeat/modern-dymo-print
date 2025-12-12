1ad9c8d43387ac17e6ab8dca659004a9091691e2
CtrlHaltDefeat <71138949+CtrlHaltDefeat@users.noreply.github.com>
2025-12-12 17:38:32 +0100

    feat: Export createLabelRenderParamsXml, createPrintParamsXml from src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { default as DymoLabel } from "@/label/DymoLabel.ts";
 export { DymoPrinter } from "@/printers/index.ts";
 export { default as DymoPrintService } from "@/printService/DymoPrintService.ts";
+export {
+	createLabelRenderParamsXml,
+	createPrintParamsXml,
+} from "@/parameter/index.ts";
---
+++ src/parameter/index.ts
@@ -0,0 +1,2 @@
+export { default as createLabelRenderParamsXml } from "@/parameter/createLabelRenderParamsXml.ts";
+export { default as createPrintParamsXml } from "@/parameter/createPrintParamsXml.ts";
